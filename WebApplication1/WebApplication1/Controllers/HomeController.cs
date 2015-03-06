905ea50f99d6e21160b616268290bcf2e9c52f4f
Sergio Alifano <sergio.alifano@gmail.com>
2015-03-06 13:15:12 -0700

    form

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
 
 namespace WebApplication1.Controllers
 {
@@ -20,11 +21,51 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Contact(Models.ContactForm contactForm)
         {
-            ViewBag.Message = "Your contact page.";
+            //add the form to the db
+            try
+            {
+                //set the date
+                contactForm.DateCreated = DateTime.Now;
 
-            return View();
+                //add something to db
+                //1.create data context
+                Models.ContactFormEntities db = new Models.ContactFormEntities();
+
+                //2. Add obj to the table
+                db.ContactForms.Add(contactForm);
+
+                //3.save
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View("Error");
+            }
+
+            //mail us a copy
+
+            MailMessage mail = new MailMessage();
+            mail.To.Add(contactForm.Email);
+
+            mail.From = new MailAddress(contactForm.Email);
+
+            mail.Subject = "Test Mail";
+
+            mail.Body = contactForm.MessageBox;
+
+            SmtpClient SmtpServer = new SmtpClient("mail.dustinkraft.com");
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("postmaster@dustinkraft.com", "techIsFun1");
+            SmtpServer.EnableSsl = false;
+
+            SmtpServer.Send(mail);
+            //redirect the user to the thank you page
+            return RedirectToAction("contact");
+            //return View();
         }
     }
 }
